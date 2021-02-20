c03ed0a7e8fdafed466dbe98b0d626a149f586f0
Ayhan <abedirhan@hotmail.com>
2021-02-19 20:02:54 -0500

    Final for today

--- Assets/_Scripts/HealthBarScreenSpaceController.cs
+++ Assets/_Scripts/HealthBarScreenSpaceController.cs
@@ -24,10 +24,18 @@ public class HealthBarScreenSpaceController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(10);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
     }
 
-    public void TakeDemage(int demage)
+    public void TakeDamage(int demage)
     {
         healthBarSlider.value -= demage;
         currentHealt -= demage;
